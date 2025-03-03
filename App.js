180cb5e20fa9b476c356dc596987a95f7cc34281
Hatice AKGÜL <katrancihatice@icloud.com>
2025-03-03 13:45:44 +0300

    Add loading

--- App.js
+++ App.js
@@ -1,29 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, SafeAreaView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, FlatList, ActivityIndicator } from 'react-native';
 import axios from 'axios';
 import Usercard from './src/components/UserCard/UserCard';
 
 export default function App() {
   const [userList, setUserList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const URL = 'https://jsonplaceholder.typicode.com/users'
 
   async function fetchData() {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+  
+    // setLoading(true);
+    // const response = await axios.get(URL);
 
-    setUserList(response.data);
+    // setLoading(false);
+
+    // setUserList(response.data);
+
+    axios.get(URL).then((response) => {
+      setLoading(false);
+      setUserList(response.data);
+
+    })
+
+
+
 
   }
 
-  const renderUser = ({ item }) => <Usercard name= {item.name} />;
+  const renderUser = ({ item }) => <Usercard name={item.name} username={item.username} email={item.name} />;
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+
 
   return (
 
     <SafeAreaView>
       <View >
-        <FlatList
-          data={userList}
-          renderItem={renderUser}
-        ></FlatList>
+        {loading ?  
+          (
+          <ActivityIndicator size="large" color="#0000ff" />
+          )
+         :
+          (
+            <FlatList data={userList} renderItem={renderUser} ></FlatList>
+          )
+        }
         <Button title="Fetch Data" onPress={fetchData} />
       </View>
 
--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -4,8 +4,16 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 
 const Usercard = props => {
     return (
-        <View style={styles.container}> 
-            <Text>Name: {props.name}</Text>
+        <View style={styles.container}>
+            <Text style={styles.username}>Name: {props.username}</Text>
+
+            <View style={styles.inner_container}>
+                <Text>Name: {props.name}</Text>
+                <Text style={styles.email}>Email: {props.email}</Text>
+
+            </View>
+
+
         </View>
     )
 }
@@ -15,6 +23,20 @@ const styles = StyleSheet.create({
         backgroundColor: '#eceff1',
         marginVertical: 10,
         padding: 10,
+    },
+    email: {
+        fontSize: 12,
+        fontStyle: 'italic',    
+        color: 'gray ',
+        marginLeft: 10
+    },
+    username: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    inner_container: {
+        flexDirection: "row",
+        alignItems: 'center',
     }
 })
 
